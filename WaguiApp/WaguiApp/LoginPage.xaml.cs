68f378918cbea3f7e1cbe2c13549705def4eb5e9
Kzoldick <kabdel552@gmail.com>
2018-08-16 15:36:41 +0100

    Page de connexion simplifiée

---
+++ WaguiApp/WaguiApp/LoginPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WaguiApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : PopupPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ WaguiApp/WaguiApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Extensions;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Services;
+
+namespace WaguiApp
+{
+	public partial class MainPage : ContentPage
+	{
+        private LoginPage _loginPopup;
+        public MainPage()
+		{
+			InitializeComponent();
+            _loginPopup = new LoginPage();
+        }
+        private async void OnOpenPupup(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(_loginPopup);
+        }
+    }
+}
